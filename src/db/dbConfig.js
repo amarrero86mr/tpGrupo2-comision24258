f4d4975392c0d35a0e3b3cd3b4dca468abd6953d
amarrero86mr <amarrero86mr@gmail.com>
2024-06-22 21:14:42 -0300

    coneccion con database exitosa

--- src/db/dbConfig.js
+++ src/db/dbConfig.js
@@ -2,13 +2,15 @@ const mysql = require('mysql2');
 require('dotenv').config();
 // /promise
 const coneccionBD = mysql.createConnection({
-  host:'bjhtasgcrg335gemekbd-mysql.services.clever-cloud.com',
-  user:'u8gcdzylz1a1c0m4',
-  password:'GJZOWkt16dmUVmjXKjp5',
-  name:'bjhtasgcrg335gemekbd',
-  port:3306
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port:3306,
+  waitForConnections:true,
+  connectionLimit: 5,
+  queueLimit: 0
 });
-
 // const coneccionBD = mysql.createConnection({
 //   host: process.env.DB_HOST,
 //   user: process.env.DB_USER,
@@ -16,7 +18,6 @@ const coneccionBD = mysql.createConnection({
 //   database: process.env.DB_NAME,
 //   // port: 3306
 // });
-
 coneccionBD.connect((err) => {
   if (err) {
     console.error('error de coneccion con dataBase: ', err);
