09576e0a219a0e3055c5825dabd3acff0b57dec4
amarrero86mr <amarrero86mr@gmail.com>
2024-06-22 22:53:34 -0300

    agregado de metodo post en usuarios

--- src/controllers/userControler.js
+++ src/controllers/userControler.js
@@ -1,6 +1,6 @@
 const coneccionBD = require('../db/dbConfig.js');
 
-
+// metodo get solicita a la db todos los usuarios
 const getAllUser = (req, res) => {
     const sql = 'SELECT * FROM usuario_tbl';
     coneccionBD.query(sql, (err, results) => {
@@ -12,7 +12,7 @@ const getAllUser = (req, res) => {
         res.json(results);
     });
 };
-
+// metodo get solicita a la db un usuario por parametro (id)
 const getAllUserByID = (req, res) => {
     const {id} = req.params;
     const sql = 'SELECT * FROM usuario_tbl WHERE id_usuario = ?'
@@ -24,7 +24,29 @@ const getAllUserByID = (req, res) => {
     });
 };
 
-module.exports = {getAllUser, getAllUserByID}
+const postNewUser = (req,res) => {
+    const {admin_user, nombre, apellido, tipo_documento, num_documento, genero, tel, email, pass } = req.body;
+
+    const sql = 'INSERT INTO usuario_tbl (admin_user, nombre, apellido, tipo_documento, num_documento, genero, tel, email, pass) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)';
+    
+    coneccionBD.query(sql, [admin_user, nombre, apellido, tipo_documento, num_documento, genero, tel, email, pass], (err, result) => {
+    
+        if (err) {
+            console.error('Error inserting user:', err);
+            return res.status(500).json({ error: 'Error creating user' });
+        }
+        
+        res.json({
+                mensaje : "Usuario Creado con EXITO",
+                idUsuario : result.insertId
+            });
+    });    
+};
+
+
+
+
+module.exports = {getAllUser, getAllUserByID, postNewUser}
 
 // exports.getAllUser = async (req, res) => {
 //     try {
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,7 +4,7 @@ const userControler = require('../controllers/userControler.js');
 
 router.get('/', userControler.getAllUser);
 router.get('/:id', userControler.getAllUserByID);
-
+router.post('/', userControler.postNewUser);
 // deberia haber otros endponits
 
 module.exports = router;
